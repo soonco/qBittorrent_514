15209c19c444ad35723d344c5fa4224fda5e2a6e
sunkuo <sunkuo@bytedance.com>
2025-12-12 21:41:28 +0800

    feat: 删除 libtorrent 依赖

--- lib_libtorrent/1220/tools/cibuildwheel/manylinux/openssl-version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export OPENSSL_ROOT=openssl-1.1.1q
-export OPENSSL_HASH=d7939ce614029cdff0b6c20f0e2e5703158a489a72b2507b8bd51bf8c8fd10ca
-export OPENSSL_DOWNLOAD_URL=https://www.openssl.org/source
